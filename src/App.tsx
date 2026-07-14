72647a36ea7ef1f34af922b649d1dba41413db71
gr-grodov <makster2323@gmail.com>
2026-07-14 12:08:40 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Card, CardHeader } from '@/components/ui/card'
+
+function App() {
+  return (
+    <>
+      <Card>
+        <CardHeader>asdasd</CardHeader>
+        <Card></Card>
+      </Card>
+    </>
+  )
+}
+
+export default App
